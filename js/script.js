68f9eb556d9310bd48763c035365791fa2e0b803
PariSolunke <60562603+PariSolunke@users.noreply.github.com>
2020-09-24 00:43:33 -0500

    V2
    Added tooltip
    Added Colours for states based on gun deaths/population

--- js/script.js
+++ js/script.js
@@ -9,59 +9,117 @@
     
 
   var
-  width=1200,height=700,
+  width=1000,height=500,
   states = topojson.feature(us, us.objects.states),
   projection = d3.geoAlbersUsa(),
   path = d3.geoPath(projection);
 
   
-  console.log(states.features[i].properties.name);
+  d3.csv("freq_by_state.csv").then(function(data) {
+   
+    
+    // Load GeoJSON data and merge with states data
+    
+    
+    // Loop through each state data value in the .csv file
+    for (var i = 0; i < data.length; i++) {
+      // Find the corresponding state inside the GeoJSON
+      for (var j = 0; j < states.features.length; j++)  {
+            
+        if (data[i].name == states.features[j].properties.name) 
+        {
+        // Copy the data value into the JSON
+        states.features[j].properties.rate = data[i].rate; 
+        states.features[j].properties.ratem = data[i].ratem;  
+        states.features[j].properties.ratef = data[i].ratef;
+        states.features[j].properties.males = data[i].males; 
+        states.features[j].properties.females = data[i].females; 
+        states.features[j].properties.total = data[i].total; 
+        break;
+        }
+        
+      }
+      
+    }});
+      
+
+var colors = d3.scaleLinear()
+  .domain([0, 15])
+  .range(['#CCCCCC','#000000']);
+
+
+setTimeout(() => {
+  var tooltip = d3.select('body')
+.append('div')
+.style('position', 'absolute')
+.style('padding', '0 10px')
+.style('background', 'white')
+.style('opacity', 0);
+
+
+  var zoom = d3.zoom()
+  .scaleExtent([1, 8])
+  .on("zoom", zoomed);
   
+  var active = d3.select(null);
   var svg = d3.selectAll("#viz").append("svg")
   .attr("width", 1300)
   .attr("height", height)
-  .on("click", reset);
+  .on("click", stopped, true);
+
+
 
 var st= svg.append("g")
-.append("g")
 
-.attr("cursor", "pointer")
-.selectAll("path")
+
+svg.call(zoom); // delete this line to disable free zooming
+    // .call(zoom.event); // not in d3 v4
+
+
+st.selectAll("path")
 .data(states.features)
-.attr("fill", "#444")
-.join("path")
+.enter()
+
+.append("path")
 .on("click", clicked)
-.attr("d", path);
 
-st.append("title")
-      .text(d => d.properties.name);
+.attr("d", path)
+.attr("fill", (d) => {
+      console.log(d.properties.rate);
+    return colors(d.properties.rate) ;
+    }
+    
+)
+.on('mouseover', function(d) {
+  tooltip.transition().duration(200)
+    .style('opacity', .9)
+    .style('pointer-events', 'none')
+
+  tooltip.html(
+    '<div style="font-weight: bold">' +'State:'+d.properties.name+'<br>Total Victims:'+d.properties.total+
+     '<br>Males:'+ d.properties.males + '<br>Females: '+d.properties.females +'</div>'
+  )
+    .style('left', (d3.event.pageX +50) + 'px')
+    .style('top', (d3.event.pageY -30) + 'px')
+  tempColor = this.style.fill;
+  d3.select(this)
+    .style('fill', 'yellow')
+})
+.on('mouseout', function(d) {
+  tooltip.html('')
+  d3.select(this)
+    .style('fill', tempColor)
+});
+;
 
 
 
 
-
-  /*
-  var svg = d3.selectAll("#viz").append("svg")
-    				.attr("width", 1300)
-            .attr("height", height);
-           
-          
-
-            svg.selectAll('path').remove();
-	// add states from topojson
-	svg.selectAll('.states')
-  .data(states.features)
-  .join('path')
-  .attr('d', f => path(f))
-  .attr('stroke', 'grey')
-  .attr('fill', 'none')
-  .attr('stroke-width', 1);
-*/
     
   
   d3.csv("freq.csv").then( function(data) {
 
-    svg.selectAll("circle")
+    st.selectAll("circle")
       .data(data)
       .enter()
       .append("circle")
@@ -73,15 +131,15 @@ st.append("title")
       })
       .attr("r", function(d) {
         if((d.males+d.females)<30)
-        return 2;
+        return 2.5;
         else if((d.males+d.females)<100)
-        return 3.15;
+        return 3.5;
         else if((d.males+d.females)<200)
-        return 4.3;
+        return 4.5;
         else if((d.males+d.females)<300)
         return 5.5;
         else
-        return 8;
+        return 6.5;
       })
         .style("fill", "rgb(217,91,67)")	
         .style("opacity", 0.60)
@@ -89,27 +147,53 @@ st.append("title")
       .text(d => d.city);	
       });
 
-  
-      function reset() {
-        st.transition().style("fill", null);
-        svg.transition().duration(750).call(
-          zoom.transform,
-          d3.zoomIdentity,
-          d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
-        );
-      }
-
-        function clicked(event, d) {
-          var [[x0, y0], [x1, y1]] = path.bounds(d);
-          d3.event.stopPropagation();
-          st.transition().style("fill", null);
-          d3.select(this).transition().style("fill", "blue");
-          
-        }
-      
+      function clicked(d) {
+        if (active.node() === this) return reset();
+        active.classed("active", false);
+        active = d3.select(this).classed("active", true);
         
       
+        var bounds = path.bounds(d),
+            dx = bounds[1][0] - bounds[0][0],
+            dy = bounds[1][1] - bounds[0][1],
+            x = (bounds[0][0] + bounds[1][0]) / 2,
+            y = (bounds[0][1] + bounds[1][1]) / 2,
+            scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
+            translate = [width / 2 - scale * x, height / 2 - scale * y];
 
+            st.transition().style("fill", null);
+            d3.select(this).transition().style("fill", "red");
+        svg.transition()
+            .duration(750)
+            // .call(zoom.translate(translate).scale(scale).event); // not in d3 v4
+            .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) ); // updated for d3 v4
+      }
+      
+      function reset() {
+        active.classed("active", false);
+        active = d3.select(null);
+      
+        svg.transition()
+            .duration(750)
+            // .call( zoom.transform, d3.zoomIdentity.translate(0, 0).scale(1) ); // not in d3 v4
+            .call( zoom.transform, d3.zoomIdentity ); // updated for d3 v4
+      }
+      
+      function zoomed() {
+        st.style("stroke-width", 1.5 / d3.event.transform.k + "px");
+        // g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"); // not in d3 v4
+        st.attr("transform", d3.event.transform); // updated for d3 v4
+      }
+      
+      // If the drag behavior prevents the default click,
+      // also stop propagation so we don’t click-to-zoom.
+      function stopped() {
+        if (d3.event.defaultPrevented) d3.event.stopPropagation();
+      }
+      
+       
+      
+    }, 1000);
 
   });
   
