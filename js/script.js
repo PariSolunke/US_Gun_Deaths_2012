506adc9cc2dae825db3b968fd81a350e5eaf0d3d
PariSolunke <60562603+PariSolunke@users.noreply.github.com>
2020-10-15 14:13:36 -0500

    Final

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-
-
-
-
-
-
-  d3.json('top.json').then(function(us) 
+d3.json('top.json').then(function(us) 
   {
     
 
@@ -47,7 +41,7 @@
       
 
 var colors = d3.scaleLinear()
-  .domain([0, 15])
+  .domain([0, 10])
   .range(['#CCCCCC','#000000']);
 
 
@@ -71,7 +65,7 @@ setTimeout(() => {
   .attr("height", height)
   .style("margin","0%")
   .style("margin-right","2.5%")
-  .style("margin-top","35")
+  .style("margin-top","8")
   
   .style("background",'#fcfce3')
   .on("click", stopped, true);
@@ -90,13 +84,11 @@ svg.call(zoom); // delete this line to disable free zooming
 st.selectAll("path")
 .data(states.features)
 .enter()
-
 .append("path")
 .on("click", clicked)
-
 .attr("d", path)
 .attr("fill", (d) => {
-    return colors(d.properties.ratem) ;
+    return colors(d.properties.rate) ;
     }
     
 )
@@ -104,7 +96,6 @@ st.selectAll("path")
   tooltip.transition().duration(200)
     .style('opacity', .75)
     .style('pointer-events', 'none')
-
   tooltip.html(
     '<div style="font-weight: bold">' +'State: '+d.properties.name+'<br>Total Victims: '+d.properties.total+
      '<br>Males: '+ d.properties.males + '<br>Females: '+d.properties.females + '<br>Aged <10: '+d.properties.age1+ '<br>Aged 13-17: '+d.properties.age2+ '<br>Aged 18+: '+d.properties.age3+     '</div>'
@@ -117,31 +108,104 @@ st.selectAll("path")
     tempOpc = this.style.opacity; 
     d3.select(this)
     .style('opacity', '0.5');
-    }
-  
-    
+    }  
 })
 .on('mouseout', function(d) {
   tooltip.html('')
-  
   if(active.node() != this)
     {
    d3.select(this)
     .style('opacity', tempOpc)}
     else{
-
       d3.select(this)
     .style('opacity', '0.3')
     }
 });
 
+dispLegend();
 
 
+function dispLegend() {
+
+  var colors = d3.scaleLinear()
+  .domain([0, 10])
+  .range(['#CCCCCC','#000000']);
+
+  var legendWidth = 100,
+      legendHeight = 400;
+
+  var svg = d3.select("#viz>svg")
+ 
+  var leg = svg.append("g")
+    .append("defs")
+      .append("svg:linearGradient")
+      .attr("id", "gradientLegend")
+      .attr("x1", "100%")
+      .attr("y1", "0%")
+      .attr("x2", "100%")
+      .attr("y2", "100%")
+      .attr("spreadMethod", "pad");
+
+  leg.append("stop")
+      .attr("offset", "0%")
+      .attr("stop-color", colors(10))
+      .attr("stop-opacity", 1);
+
+  leg.append("stop")
+      .attr("offset", "100%")
+      .attr("stop-color", colors(0))
+      .attr("stop-opacity", 1);
+
+  svg.append("rect")
+      .attr("width", 30)
+      .attr("height", legendHeight)
+      .style("fill", "url(#gradientLegend)")
+      .attr("transform", "translate(10,10)")
+
+  var y = d3.scaleLinear()
+      .range([legendHeight, 0])
+      .domain([0, 10]);
+
+  var yAxis = d3.axisRight(y);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(41,12)")
+      .call(yAxis);
+
+     allCities("all");
 
 
-    
-  
-  d3.csv("freq.csv").then( function(data) {
+     d3.selectAll("input[name='type']").on("change", function(){
+      if(this.value=="gender"){
+        
+     allCities("all");
+      }
+      else
+      normCities();
+  });
+  d3.selectAll("select[name='Genders']").on("change", function()
+  {
+   if(this.value=="all")
+    allCities("all");
+    else if(this.value=="male")
+    allCities("male");
+    else if(this.value=="female")
+    allCities("female");
+
+  });
+
+
+}
+
+function normCities()
+{
+  d3.select('#genderlabel').style("visibility","hidden");
+  d3.select('#genderselect').style("visibility","hidden");
+  d3.selectAll("circle").remove();
+
+
+  d3.csv("30cities.csv").then( function(data) {
 
     st.selectAll("circle")
       .data(data)
@@ -154,6 +218,73 @@ st.selectAll("path")
         return projection([d.lng, d.lat])[1];
       })
       .attr("r", function(d) {
+        if((d.rate)<5)
+        return 2;
+        else if((d.rate)<15)
+        return 4.5;
+        else if((d.rate)<30)
+        return 6.5;
+        else if((d.rate)<40)
+        return 8.2;
+        else
+        return 9;
+      })
+        .style("fill", "#DC3220")	
+        .style("opacity", '0.9')
+        .on("click",comparison)
+        .on('mouseover', function(d) {
+          
+          tooltip.transition().duration(200)
+            .style('opacity', .9)
+            .style('pointer-events', 'none')
+        
+          tooltip.html(
+            '<div style="font-size: 0.8rem; font-weight: bold">' +'City: '+d.city+'<br>RATE: '+d.rate+'<br>Total Victims: '+d.total+
+             '<br>Males: '+ d.males + '<br>Females: '+d.females +'<br>Aged <10: '+d.age1+ '<br>Aged 13-17: '+d.age2+ '<br>Aged 18+: '+d.age3+'</div>'
+          )
+            .style('left', (d3.event.pageX +50) + 'px')
+            .style('top', (d3.event.pageY -30) + 'px')
+          
+          d3.select(this)
+            .style('opacity', '1')
+        })
+        .on('mouseout', function(d) {
+          tooltip.html('')
+          d3.select(this)
+            .style('opacity', '0.7')
+        });	
+      });
+  
+
+}
+
+
+
+
+
+    
+function allCities(curFilter)
+{
+  d3.selectAll("circle").remove();
+
+  d3.select('#genderlabel').style("visibility","visible");
+  d3.select('#genderselect').style("visibility","visible");
+  d3.csv("freq.csv").then( function(data) {
+
+    st.selectAll("circle")
+      .data(data)
+      .enter()
+      
+      .append("circle")
+      .attr("cx", function(d) {
+        return projection([d.lng, d.lat])[0];
+      })
+      .attr("cy", function(d) {
+        return projection([d.lng, d.lat])[1];
+      })
+      .attr("r", function(d) {
+        if(curFilter=="all")
+       {
         if((d.total)<20)
         return 1.2;
         else if((d.total)<50)
@@ -164,6 +295,33 @@ st.selectAll("path")
         return 8.5;
         else
         return 9.5;
+       }
+       else if(curFilter=="male")
+       {
+        if((d.males)<20)
+        return 1.2;
+        else if((d.males)<50)
+        return 3;
+        else if((d.males)<200)
+        return 5.5;
+        else if((d.males)<300)
+        return 8.5;
+        else
+        return 9.5;
+       }
+       else if(curFilter=="female")
+       {
+        if((d.females)<20)
+        return 1.2;
+        else if((d.females)<50)
+        return 3;
+        else if((d.females)<200)
+        return 5.5;
+        else if((d.females)<300)
+        return 8.5;
+        else
+        return 9.5;
+       }
       })
         .style("fill", "#DC3220")	
         .style("opacity", '0.7')
@@ -190,7 +348,7 @@ st.selectAll("path")
             .style('opacity', '0.7')
         });	
       });
-
+    }
 
       function comparison(d)
       { 
@@ -424,7 +582,7 @@ sidesvg2.selectAll("mybar2")
       
        
       
-    }, 1000);
+    }, 1150);
 
   });
   
@@ -432,148 +590,3 @@ sidesvg2.selectAll("mybar2")
   
   
   
-/*
-
-
-
-  
-/*
-  map = d3.select('#viz').append('svg')
-.attr('width', width + margin.left + margin.right)
-.attr('height', height + margin.top + margin.bottom)
-
-// add states from topojson
-map.selectAll("path")
-.data(states).enter()
-.append("path")
-.attr("class", "feature")
-.style("fill", "steelblue")
-.attr("d", path);
-
-// put boarder around states 
-map.append("path")
-.datum(topojson.mesh(topo, topo.objects.states, function(a, b) { return a !== b; }))
-.attr("class", "mesh")
-.attr("d", path);
-*/
-
-
-
-  /*
-
-var   tempColor,
-    yScale,
-    yAxisValues,
-    yAxisTicks,
-    yGuide,
-    xScale,
-    xAxisValues,
-    xAxisTicks,
-    xGuide,
-    colors,
-    tooltip,
-    myChart;
-
-for (var i = 0; i<d.list.length; i++) {
-temperatures.push(d.list[i].main.temp);
-dates.push( new Date(d.list[i].dt_txt) );
-}
-
-yScale = d3.scaleLinear()
-.domain([0, d3.max(temperatures)])
-.range([0,height]);
-
-yAxisValues = d3.scaleLinear()
-.domain([0, d3.max(temperatures)])
-.range([height,0]);
-
-yAxisTicks = d3.axisLeft(yAxisValues)
-.ticks(10)
-
-xScale = d3.scaleBand()
-.domain(temperatures)
-.paddingInner(.1)
-.paddingOuter(.1)
-.range([0, width])
-
-xAxisValues = d3.scaleTime()
-.domain([dates[0],dates[(dates.length-1)]])
-.range([0, width])
-
-xAxisTicks = d3.axisBottom(xAxisValues)
-.ticks(d3.timeDay.every(1))
-
-colors = d3.scaleLinear()
-.domain([0, 65, d3.max(temperatures)])
-.range(['#FFFFFF', '#2D8BCF', '#DA3637'])
-
-tooltip = d3.select('body')
-.append('div')
-.style('position', 'absolute')
-.style('padding', '0 10px')
-.style('background', 'white')
-.style('opacity', 0);
-
-myChart = d3.select('#viz').append('svg')
-.attr('width', width + margin.left + margin.right)
-.attr('height', height + margin.top + margin.bottom)
-.append('g')
-.attr('transform',
-  'translate(' + margin.left + ',' + margin.right + ')')
-.selectAll('rect').data(temperatures)
-.enter().append('rect')
-  .attr('fill', colors)
-  .attr('width', function(d) {
-    return xScale.bandwidth();
-  })
-  .attr('height', 0)
-  .attr('x', function(d) {
-    return xScale(d);
-  })
-  .attr('y', height)
-  
-  .on('mouseover', function(d) {
-    tooltip.transition().duration(200)
-      .style('opacity', .9)
-    tooltip.html(
-      '<div style="font-size: 2rem; font-weight: bold">' +
-        d + '&deg;</div>'
-    )
-      .style('left', (d3.event.pageX -35) + 'px')
-      .style('top', (d3.event.pageY -30) + 'px')
-    tempColor = this.style.fill;
-    d3.select(this)
-      .style('fill', 'yellow')
-  })
-
-  .on('mouseout', function(d) {
-    tooltip.html('')
-    d3.select(this)
-      .style('fill', tempColor)
-  });
-
-yGuide = d3.select('#viz svg').append('g')
-        .attr('transform', 'translate(20,0)')
-        .call(yAxisTicks)
-
-xGuide = d3.select('#viz svg').append('g')
-        .attr('transform', 'translate(20,'+ height + ')')
-        .call(xAxisTicks)
-
-myChart.transition()
-.attr('height', function(d) {
-  return yScale(d);
-})
-.attr('y', function(d) {
-  return height - yScale(d);
-})
-.delay(function(d, i) {
-  return i * 20;
-})
-.duration(1000)
-.ease(d3.easeBounceOut)
-
-*/
-
-
-
